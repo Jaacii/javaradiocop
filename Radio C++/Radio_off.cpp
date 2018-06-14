806250b0f3830cfacc891d086959cd9369ded1f2
fullpotat0 <nils.s13@gmx.de>
2018-06-14 09:06:31 +0200

    Radio C++

---
+++ Radio C++/Radio_off.cpp
@@ -0,0 +1,21 @@
+#include "../Si4703_Breakout.h"
+#include <stdio.h>
+
+int main()
+{
+        int resetPin = 23; //GPIO_23
+        int sdaPin = 0; //GPIO_0 (SDA)
+
+        Si4703_Breakout radio(resetPin, sdaPin);
+        radio.powerOff();
+        //radio.setVolume(5);
+        //radio.setChannel(1036); //FM 103.6Mhz Radio Hamburg
+        
+        //char rdsBuffer[10] = {0};
+        //radio.readRDS(rdsBuffer, 15000); //timeout 15sec
+        printf("Radio aus");
+
+        //radio.printRegisters();
+
+        return 0;
+}
