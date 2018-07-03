d318745375163eddf9d402aa69f1fcecd281cad0
Ehlen <Joel.Ehlen@haw-hamburg.de>
2018-07-03 15:24:03 +0200

    testradio

--- Radio C++/Radio.cpp
+++ Radio C++/Radio.cpp
@@ -3,19 +3,67 @@
 
 int main()
 {
+		int volume = 5;
         int resetPin = 23; //GPIO_23
         int sdaPin = 0; //GPIO_0 (SDA)
 
         Si4703_Breakout radio(resetPin, sdaPin);
         radio.powerOn();
-        radio.setVolume(5);
-        radio.setChannel(1036); //FM 103.6Mhz Radio Hamburg
+        volumeChange(0);
+		
+		channelSelect();
+        //radio.setChannel(1036); //FM 103.6Mhz Radio Hamburg
         
         char rdsBuffer[10] = {0};
         radio.readRDS(rdsBuffer, 15000); //timeout 15sec
-        printf("Listening to station: %s %.1f\n", rdsBuffer, radio.getChannel()/10.0);
+       // printf("Listening to station: %s %.1f\n", rdsBuffer, radio.getChannel()/10.0);
 
         radio.printRegisters();
 
         return 0;
+}
+
+void channelSelect() {
+	
+	int selectStation = 0;
+	
+	if (selectStation == 0) {	//radio HH
+	radio.setChannel(1036);
+	
+	printf("Listening to station: Radio Hamburg");
+	selectStation++;
+	}
+	else if  (selectStation == 1) {	//Nrd?
+	radio.setChannel(876);
+	
+	printf("Listening to station: Ndr2");
+	selectStation++;
+	}
+	
+	else {
+	radio.setChannel(942);		//N joy
+	
+	printf("Listening to station: NJoy");
+	selectStation = 0;
+	}
+	
+	
+	return 0;
+}
+
+void volumeChange(int changer) {
+	int changer = this.changer;
+	
+	if (changer == 0) {						// simple setVolume
+		radio.setVolume(volume);
+	}
+	else if (changer == 1) {
+		volume = volume + 5;
+		radio.setVolume(volume);			//+ Volume
+	}
+	else (changer == 2) {					//- Volume
+		volume = volume - 5;
+		radio.setVolume(volume);
+	}
+	
 }
